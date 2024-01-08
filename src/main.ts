956441ea7945f2eef3662df20b3737be0f87016d
Ishan Ranasinghe <is.ranasinghe@sana-commerce.com>
2024-01-08 19:05:19 +0530

    added helmat and cores

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+  app.use(helmet());
+
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Backend API')
     .setDescription('All available API as follows')
