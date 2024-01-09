f65670ef355d7b88a8824dd0d4c586d3225fb300
Ishan Ranasinghe <is.ranasinghe@sana-commerce.com>
2024-01-09 11:26:30 +0530

    added cores

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
   app.use(helmet());
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://example.com',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('Backend API')
     .setDescription('All available API as follows')
